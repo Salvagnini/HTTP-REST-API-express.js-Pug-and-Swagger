a543c0ffc0f0de4a486e806050f3ac84594a0490
Dmytro_Nazarchuk <dmutro.nz@gmail.com>
2023-04-10 13:43:26 +0300

    fin2

--- index.js
+++ index.js
@@ -19,15 +19,15 @@ const app = express();
 
 
 // ******************************   1   *************************
-app.get('/', (req, res) => {
-  res.send('<h1>Welcome to our shop</h1>');
-});
+// app.get('/', (req, res) => {
+//   res.send('<h1>Welcome to our shop</h1>');
+// });
 
-app.get('/product', (req, res) => {
-  let title = req.query?.title || 'Title';
-  const price = req.query?.price || 'Price';
-  res.send(`<h1>${title}</h1>  <h2>For just ${price}</h2>`);
-});
+// app.get('/product', (req, res) => {
+//   let title = req.query?.title || 'Title';
+//   const price = req.query?.price || 'Price';
+//   res.send(`<h1>${title}</h1>  <h2>For just ${price}</h2>`);
+// });
 
 
 // http://localhost:8000/product?title=Iphone&price=999.99   задати в url
@@ -35,37 +35,39 @@ app.get('/product', (req, res) => {
 
 
 // *******************   2   ***********
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname + '/views/index.html'));
-})
+// app.get('/', (req, res)=>{
+//     res.sendFile(path.join(__dirname + '/views/index.html'));
+// })
 
 
 
 // *********************   3  ***********************
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
+// app.set('view engine', 'pug');
 
-app.set('view engine', 'pug');
+// app.get('/', (req, res)=>{
+//     res.sendFile(path.join(__dirname + '/views/index.html'));
+// })
 
-app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname + '/views/index.html'));
-})
-
-app.get('/product', (req, res)=>{
-    const title = req.query?.title || '';
-    const price = req.query?.price || '';
+// app.get('/product', (req, res)=>{
+//     const title = req.query?.title || '';
+//     const price = req.query?.price || '';
    
-    res.render('product', {product_title: title, product_price: price})
-    // res.send(`<h1> ${title} </h1> <h2> For just ${price} </h2>`);
-})
+//     res.render('product', {product_title: title, product_price: price})
+//     // res.send(`<h1> ${title} </h1> <h2> For just ${price} </h2>`);
+// })
 
 
 
 
 // ******************   4   **************
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const require = createRequire(import.meta.url);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(require('./swagger.json')));
 
